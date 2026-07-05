cc77588c664500d5131df110b0ccea6e71227c7c
Sagar29081993 <ayare.s@northeastern.edu>
2026-07-05 14:05:47 +0400

    Add Model Code

--- model.py
+++ model.py
@@ -1,1 +1,18 @@
+import tensorflow as tf
 
+def build_model(vocab_size=10000, max_length=200):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, 64, input_length=max_length),
+        tf.keras.layers.GlobalAveragePooling1D(),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.5),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="binary_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
