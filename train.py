60dc1eee2b4dcb1fd253c369118466de75420167
Sagar29081993 <ayare.s@northeastern.edu>
2026-07-05 14:06:54 +0400

    ADD Model Code

--- train.py
+++ train.py
@@ -1,1 +1,23 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import imdb
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from model import build_model
+import matplotlib.pyplot as plt
 
+vocab_size = 10000
+max_length = 200
+
+(x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=vocab_size)
+
+x_train = pad_sequences(x_train, maxlen=max_length)
+x_test = pad_sequences(x_test, maxlen=max_length)
+
+model = build_model(vocab_size, max_length)
+
+history = model.fit(
+    x_train,
+    y_train,
+    epochs=10,
+    batch_size=32,
+    validation_split=0.2
+)
